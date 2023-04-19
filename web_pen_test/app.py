4cab153de432b4459a4b95a79608e9c18bdc12cc
anthoniraj <anthoniraja@gmail.com>
2023-04-20 04:06:27 +0530

    updated flask app

--- web_pen_test/app.py
+++ web_pen_test/app.py
@@ -1,12 +1,10 @@
 from flask import Flask, jsonify
-import sqlite3
 
 '''
 Author: Anthoniraj Amalanathan
 Date Last Modified: 19-Apr-2023
 Description: Flask Web Services for Demonstrating the following Web Penetration Testing
 1. Bruteforce Login Attack using Hydra
-SQL Injection
 '''
 
 app = Flask(__name__)
@@ -29,18 +27,6 @@ def verify_login(username, password):
         return "Welcome to Admin Portal"
     else:
         return "Unauthorised User"
-
-@app.route("/user/<string:name>")
-def search_user(name):
-    '''
-    SQL Injection Attack
-    '''
-    con = sqlite3.connect("test.db")
-    cur = con.cursor()
-    cur.execute("select * from test where username = '%s'" % name)
-    data = str(cur.fetchall())
-    con.close()    
-    return jsonify(data=data),200
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0",port=8080, debug=True)
